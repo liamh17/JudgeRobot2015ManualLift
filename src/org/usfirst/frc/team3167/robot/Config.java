1e0d939cc059ee171c1d7b27a4da750284f02d90
Mark Macerato <15mmacerato@fatherjudgestudent.com>
2015-01-29 14:38:59 -0500

    Create Config.java

---
+++ src/org/usfirst/frc/team3167/robot/Config.java
@@ -0,0 +1,8 @@
+/**
+ *  A class solely to hold global constants needed throughout the program, such as
+ *  motor properties, pins that certain sensors are plugged into, or PID constants
+ */
+public class Config
+{
+
+} 
