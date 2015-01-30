0e9814f62fdbe721785f8a2880ca3dd44d5c9840
Mark Macerato <15mmacerato@fatherjudgestudent.com>
2015-01-30 09:47:55 -0500

    Updated Configuration class

--- src/org/usfirst/frc/team3167/robot/RobotConfiguration.java
+++ src/org/usfirst/frc/team3167/robot/RobotConfiguration.java
@@ -2,7 +2,7 @@
  *  A class solely to hold global constants needed throughout the program, such as
  *  motor properties, pins that certain sensors are plugged into, or PID constants
  */
-public class Config
+public class RobotConfiguration
 {
 
 } 
