53917945855b6e14d717daa996ecd33e951188db
Mark Macerato <15mmacerato@fatherjudgestudent.com>
2015-01-30 11:55:25 -0500

    Update and rename VisionPacket.java to PiVision.java

---
+++ src/org/usfirst/frc/team3167/networking/PiVision.java
@@ -0,0 +1,26 @@
+/**
+ * Team 3167, Father Judge High School
+ */
+
+package org.ufirst.frc.team3167.networking;
+
+import java.net.DatagramPacket;
+ 
+ /**
+  * Class used to receive datagram packets from the raspberry pi for target tracking.
+  * The class uses UDP to receive packets holding data about the target being tracked by
+  * the Pi's vision code.
+  */
+public class PiVision
+{
+    private int port;
+    private DatagramSocket socket;
+    private byte[] buffer;
+    
+    public PiVision(int port)
+    {
+        this.port = port;
+        socket = new DatagramSocket(port);
+        buffer = new byte[2048];
+    }
+}
