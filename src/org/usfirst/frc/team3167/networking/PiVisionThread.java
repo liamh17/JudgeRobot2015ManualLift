eba82951019a5a14b8174d0c1a3807d1c053ef57
Mark Macerato <15mmacerato@fatherjudgestudent.com>
2015-01-30 18:57:03 -0500

    Created code for networking with Pi

--- src/org/usfirst/frc/team3167/networking/PiVisionThread.java
+++ src/org/usfirst/frc/team3167/networking/PiVisionThread.java
@@ -4,31 +4,51 @@
 
 package org.usfirst.frc.team3167.networking;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketException;
  
  /**
   * Class used to receive datagram packets from the raspberry pi for target tracking.
   * The class uses UDP to receive packets holding data about the target being tracked by
   * the Pi's vision code.
   */
-public class PiVision
+public class PiVisionThread extends Thread
 {
     private int port;
     private DatagramSocket socket;
     private DatagramPacket packet;
     private byte[] buffer;
+    private byte[] receivedData;
     
-    public PiVision(int port)
+    public PiVisionThread(int port)
     {
         this.port = port;
-        socket = new DatagramSocket(port);
-        buffer = new byte[2048];
+        try 
+        {
+			socket = new DatagramSocket(port);
+		} 
+        catch (SocketException e) 
+        {
+			e.printStackTrace();
+		}
+        buffer = new byte[97];
         packet = new DatagramPacket(buffer, buffer.length);
+        receivedData = new byte[97];
     }
     
     public void receive()
     {
-        socket.receive();
+        try 
+        {
+			socket.receive(packet);
+		}
+        catch (IOException e)
+        {
+			e.printStackTrace();
+		}
+        receivedData = packet.getData();
+        packet.setData(buffer);
     }
 }
