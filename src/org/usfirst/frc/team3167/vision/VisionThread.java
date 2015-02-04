8ea985e7003e8fe97e6d976475bb9c86be98a44f
Mark Macerato (Shop) <15mmacerato@fatherjudgestudent.com>
2015-02-04 14:57:40 -0500

    Filled out vision classes

--- src/org/usfirst/frc/team3167/vision/VisionThread.java
+++ src/org/usfirst/frc/team3167/vision/VisionThread.java
@@ -1,5 +1,43 @@
 package org.usfirst.frc.team3167.vision;
 
-public class VisionThread {
+import org.usfirst.frc.team3167.control.RobotPositionEstimator;
 
+import edu.wpi.first.wpilibj.vision.AxisCamera;
+
+public class VisionThread extends Thread
+{
+	public static final byte WIDE = 0;
+	public static final byte LONG = 1;
+		
+	private Tracker tracker;
+	private RobotPositionEstimator manager;
+		
+	public VisionThread(byte mode, AxisCamera camera, String templatePath, RobotPositionEstimator manager)
+	{
+		super();
+		this.manager = manager;
+		if(mode == LONG)
+		{
+			tracker = new LogoTracker(camera, templatePath);
+		}
+		else if(mode == WIDE)
+		{
+			tracker = new WideTracker(camera, templatePath);
+		}
+	}
+	
+	public void run()
+	{
+		if(tracker.seesTote())
+		{
+			manager.setSeesTote(true);;
+			tracker.doAnalysis();
+			manager.setPosition(tracker.getPosition());
+		}
+		else
+		{
+			manager.setSeesTote(false);
+		}
+	}
+	
 }
